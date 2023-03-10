ed0043deb85aa95af321f83812d39ab6dd5c92c7
marquin.e <marcos.emanuel.04@gmail.com>
2023-03-10 15:51:45 -0300

    :lipstick: Added PostgreSQL configuration, register page its now being used by the register route
    Now we have a PostgreSQL database, its configured at index.js, in the same script its the route to create a new user, fully functional. The next step its add functions to make the login page functional

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 import express from "express"
+import bodyParser from "body-parser"
+import pg from "pg"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express()
 const port = 3000
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Configurando o body parser para coleta de dados via HTTP
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Configurando o Express para usar os arquivos 
 app.use(express.static('public'))
+
+// Configurando o banco de dados
+const pool = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "users",
+    password: "admin",
+    port: "5432"
+})
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/public/register.html');
+});
+
+// Rota para criar usuário
+app.post('/register', (req, res) => {
+    const { nome, email, senha } = req.body;
+    const sql = 'INSERT INTO users (nome, email, senha) VALUES ($1, $2, $3)';
+    const values = [nome, email, senha];
+    pool.query(sql, values, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Erro ao criar usuário');
+      } else {
+        res.status(201).send('Usuário criado com sucesso');
+      }
+    });
+});
 
 app.listen(port, (error) => {
     if (error) {
